8f4a7475fb0a9ca875971220743a6d517e0bc877
Mohammad Zarchi <me@zartweety.ir>
2023-02-28 12:49:08 +0330

    Improved performance by receiving links

--- Channel/MentionMinear.py
+++ Channel/MentionMinear.py
@@ -20,15 +20,15 @@ def get_comments(client, channel, message_id):
     return data
 
 
-# https://t.me/Arzoyman33/5082
-chennel_link = "Arzoyman33"
-post_id = 5082
+post_link = "https://t.me/MohammadZarchi/111"
+cd = post_link.split('/')
+post_id = int(cd[4])
 user_ids = []
-cmd = get_comments(tclient, chennel_link, post_id)
-f = open(f"{chennel_link}-{post_id}.txt", "a")
+cmd = get_comments(tclient, cd[3], post_id)
+f = open(f"Channel/ChannelMention/{cd[3]}-{post_id}.txt", "a")
 for i, p in enumerate(cmd):
     if not cmd[p] in user_ids:
-        msg = f"{i+1} - https://t.me/{chennel_link}/{post_id}?comment={p}"
+        msg = f"{i+1} - https://t.me/{cd[3]}/{post_id}?comment={p}"
         user_ids.append(cmd[p])
         f.write(msg + "\n")
         print(msg)
