115a3f361547d275fd01eec1b9730a9b24e08dd1
Mohammad Zarchi <me@zartweety.ir>
2023-03-01 11:52:28 +0330

    List of contacts

---
+++ contacts/list.py
@@ -0,0 +1,11 @@
+import config.appconfig as ac
+from telethon.sync import TelegramClient
+from telethon import functions
+import sys
+sys.path.append('../GitHub')
+
+with TelegramClient('../sessions/my', ac.api_id, ac.api_hash) as client:
+    result = client(functions.contacts.GetContactsRequest(hash=0))
+    for i, user in enumerate(result.users):
+        txt = f"{i+1}, Id: {user.id}, Mutual: {user.mutual_contact}, Number: {user.phone}, Name: {user.first_name} {user.last_name}"
+        print(txt)
