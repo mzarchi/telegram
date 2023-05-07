f55f52d63a9cfe9cdff2933300b7812b90249b63
mzarchi <me@zartweety.ir>
2023-05-07 08:51:35 +0330

    Reformat and debug code

--- login/main.py
+++ login/main.py
@@ -1,13 +1,14 @@
 from telethon.sync import TelegramClient
 import telethon
+
 import sys
-sys.path.append('../GitHub')
-import config.appconfig as ac
+sys.path.append('./')
+import config as ac
 
 phone = input("Insert Your Phone: ")
 name = phone
 client = TelegramClient(
-    f'../sessions/{name.replace("+", "")}', ac.api_id, ac.api_hash)
+    f'sessions/{name.replace("+", "")}', ac.api_id, ac.api_hash)
 client.connect()
 client.send_code_request(phone, force_sms=False)
 value = input("Insert Login Code: ")
