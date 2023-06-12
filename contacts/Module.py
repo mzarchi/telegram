1fa548bc49f8da3d9eeb5a46156a54e2b7dff38a
mzarchi <me@zartweety.ir>
2023-06-12 12:28:15 +0330

    Fixed read session bug

--- contacts/Module.py
+++ contacts/Module.py
@@ -3,7 +3,7 @@ import sys
 
 class Config:
     def __init__(self):
-        sys.path.append(sys.path[0].replace('/contacts', '/config'))
-        import appconfig as ac
+        sys.path.append(sys.path[0].replace('/contacts', '/'))
+        import config as ac
         self.id = ac.api_id
         self.hash = ac.api_hash
--- contacts/list.py
+++ contacts/list.py
@@ -3,8 +3,9 @@ from telethon import functions
 from Module import Config
 
 cf = Config()
-with TelegramClient('../sessions/my', cf.id, cf.hash) as client:
+with TelegramClient('sessions/test', cf.id, cf.hash) as client:
     result = client(functions.contacts.GetContactsRequest(hash=0))
     for i, user in enumerate(result.users):
-        txt = f"{i+1}, Id: {user.id}, Mutual: {user.mutual_contact}, Number: {user.phone}, Name: {user.first_name} {user.last_name}"
-        print(txt)
+        if user.mutual_contact:
+            txt = f"{i+1}, Id: {user.id}, Mutual: {user.mutual_contact}, Number: {user.phone}, Name: {user.first_name} {user.last_name}"
+            print(txt)
