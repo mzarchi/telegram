59b1020453939db0f32f2bbcdbdb05936a3aaa6f
alisharify <alisharifyofficial@gmail.com>
2023-07-05 22:25:37 +0330

    Change Login method in login/main.py

--- login/main.py
+++ login/main.py
@@ -1,20 +1,63 @@
-from telethon.sync import TelegramClient
-import telethon
-
 import sys
-sys.path.append('./')
+import getpass
+import telethon
+import utils
 import config as ac
+from telethon.sync import TelegramClient
 
-phone = input("Insert Your Phone: ")
-name = phone
-client = TelegramClient(
-    f'sessions/{name.replace("+", "")}', ac.api_id, ac.api_hash)
-client.connect()
-client.send_code_request(phone, force_sms=False)
-value = input("Insert Login Code: ")
-try:
-    client.sign_in(phone, code=value)
-except telethon.errors.SessionPasswordNeededError:
-    password = input("Insert Your 2-Step Code: ")
-    client.sign_in(password=password)
-print("Successfully Connect!\nSession created at sessions dir")
+sys.path.append('./')
+
+class ClientManager:
+    """Base Class For login"""
+    __phoneNumber = None
+    __TwoFactor = None
+    __LoginCode = None
+    __Client = None
+
+    def GetPhoneNumber(self):
+        """Get User Phone Number"""
+        while True:
+            print(utils.LOGIN_MESSAGE)
+            x = input(": ")
+            if not utils.ValidatePhone(x):
+                print("Invalid Phone number :(")
+                continue
+            else:
+                self.__phoneNumber = x
+
+    def GetLoginCode(self):
+        """Get Telegram Code in Hidden Mode"""
+        self.__LoginCode = getpass.getpass("LoginCode: ")
+
+    def GetTwoStepCode(self):
+        """Get 2-Step code in hidden mode"""
+        self.__TwoFactor = getpass.getpass("2-Step Code: ")
+
+
+    def LoginClient(self):
+        """Login Method"""
+        self.GetPhoneNumber()
+        client = TelegramClient(
+            f'sessions/{self.__phoneNumber.replace("+", "")}', ac.api_id, ac.api_hash)
+        client.connect()
+        client.send_code_request(self.__phoneNumber, force_sms=False)
+        self.GetLoginCode()
+        try:
+            client.sign_in(self.__phoneNumber, code=self.__LoginCode)
+        except telethon.errors.SessionPasswordNeededError:
+            self.GetTwoStepCode()
+            client.sign_in(password=self.__TwoFactor)
+        print("Successfully Connect!\nSession created at sessions dir")
+        self.__Client = client
+
+
+    def GetClient(self):
+        """Use This Method For Getting <TelegramClient> client"""
+        return self.__Client
+
+
+
+
+
+client = ClientManager()
+client.LoginClient()
